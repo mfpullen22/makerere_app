b4eecf4c1922a1edf43d579dc1bf220e43e15774
Matthew Pullen <matthew.pullen@gmail.com>
2024-03-12 19:07:35 -0500

    Added elevatedbuttons to home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,7 +13,22 @@ class HomeScreen extends StatelessWidget {
             width: 300,
             height: 300,
             fit: BoxFit.contain,
-          )
+          ),
+          const SizedBox(height: 40),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("Orientation Materials"),
+            ),
+          ),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text("Semester Schedule"),
+            ),
+          ),
         ],
       ),
     );
