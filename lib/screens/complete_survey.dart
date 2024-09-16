45cd95807dd83bc643bbc3046174148f8fc75f9d
Matthew Pullen <matthew.pullen@gmail.com>
2024-09-16 14:49:49 -0500

    Added survey screen files to start

---
+++ lib/screens/complete_survey.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class CompleteSurveysScreen extends StatelessWidget {
+  const CompleteSurveysScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/view_surveys.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class ViewSurveysScreen extends StatelessWidget {
+  const ViewSurveysScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
