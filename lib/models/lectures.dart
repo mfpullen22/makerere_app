3feccc2f637c0fb0ebc43136f0b78d3bf2a53fe5
Matthew Pullen <matthew.pullen@gmail.com>
2025-07-25 01:33:23 -0500

    A million android version changes, not working

--- lib/models/lectures.dart
+++
@@ -1,5 +0,0 @@
-class Lecture {
-  const Lecture({required this.title, required this.fileName});
-  final String title;
-  final String fileName;
-}
--- lib/models/schedules.dart
+++
@@ -1,3 +0,0 @@
-class ScheduleFirstSecond {}
-
-class ScheduleThird {}
